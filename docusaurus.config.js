572d8680fc5aff0b833ade15348e8d11b245aff5
Idorenyin Akaninyene <82785471+Dreno2003@users.noreply.github.com>
2024-06-23 15:49:18 +0000

    update url in configue file

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,7 +11,7 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://portaone.github.io',
+  url: 'https://sinews-mobile.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/cloudpbx-portal-docs/',
